7e688e704460cc3d26dc80d5674fe398bd9cc0ee
Simon Hawker <shawker@esri.com>
2019-01-14 13:23:58 +0000

    Implemented the rest requests

--- src/festiflow-minion/app/src/main/java/com/festiflo/festiflow_minion/RestRequest.java
+++ src/festiflow-minion/app/src/main/java/com/festiflo/festiflow_minion/RestRequest.java
@@ -1,28 +1,22 @@
 //package com.festiflo.festiflow_minion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.net.HttpURLConnection;
+import java.io.*;
+import java.net.*;
 import java.util.*;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 import java.security.cert.X509Certificate;
+//import org.json.*;
 
 public class RestRequest {
   
   private static String cardiffServer = "https://cardiffportal.esri.com";
+  private static String usersLayer = "/server/rest/services/Hosted/EventServiceWebMercator/FeatureServer/0/";
+  private static String eventLayer = "/server/rest/services/Hosted/EventServiceWebMercator/FeatureServer/0/";
   
-  private static String postHttpRequest(String urlString, Map<String, Object> paramMap) {
-     try {
-      
+  public RestRequest() {
+    
+    // Our certificates are invalid, so we need to bypass certificate validation
+    try {
       TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
           public java.security.cert.X509Certificate[] getAcceptedIssuers() {
             return null;
@@ -48,10 +42,20 @@ public class RestRequest {
           public boolean verify(String hostname, SSLSession session) {
               return true;
           }
-      };      
+      };
       
+      HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }      
+  }
+    
+  private String postHttpRequest(String urlString, Map<String, Object> paramMap) {
+    try {
+      // Build and encode the post content
       StringBuilder postData = new StringBuilder();
-      for (Map.Entry<String, Object> param : params.entrySet()) {
+      for (Map.Entry<String, Object> param : paramMap.entrySet()) {
         if (postData.length() != 0) 
           postData.append('&');
         
@@ -61,7 +65,7 @@ public class RestRequest {
       }
       byte[] postDataBytes = postData.toString().getBytes("UTF-8");
       
-      HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+      // Setup the post headers and content
       URL url = new URL(urlString);
       HttpURLConnection conn = (HttpURLConnection) url.openConnection();
       conn.setRequestMethod("POST");
@@ -77,8 +81,8 @@ public class RestRequest {
         return "";
       }
 
+      // Get the output
       BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-      
       StringBuffer sb = new StringBuffer();
       String output;
       while ((output = br.readLine()) != null) {
@@ -93,9 +97,12 @@ public class RestRequest {
     
     return "";
   }
-  
-  
-  private static String getToken() {
+    
+  private String getToken() {
+    
+    return "2oxPcK8tehXJLe9B8p1FlF1cJyl9Slj7mavZkKJxflul0QfISINSgrQWAWZzn8l-KkIw6D81neGDmq4tW10j2dBZqSzKugUq-fTnk81eHmfCRUA7Vfl7SZXfQVfrHkLulI1RJcmzkk54nNNavMptF5-9Z0ktXYSYTrcCbyX9qp4.";
+    
+    /*
     String tokenUrl = "/portal/sharing/rest/generateToken/";
     
     Map<String, Object> params = new LinkedHashMap<>();
@@ -111,20 +118,51 @@ public class RestRequest {
       System.out.println("Failed to get token");
       return "";
     }
+        
+    // ToDo Parse JSON
+        
+    return result;
+    */
+  }
+  
+  public void updateUserPostion(int userID, double x, double y) {
+    String token = getToken();
     
+    Map<String, Object> queryParams = new LinkedHashMap<>();
+    queryParams.put("where", "id = " + Integer.toString(userID));
+    queryParams.put("outfields", "*");
+    queryParams.put("token", token);
+    queryParams.put("f", "pjson");
+    String userData = postHttpRequest(cardiffServer + usersLayer + "query", queryParams);
+
+    // ToDo: parse json
+    
+    String usersJSONArray = "[]";
+    
+    Map<String, Object> updateParams = new LinkedHashMap<>();
+    updateParams.put("token", token);
+    updateParams.put("f", "pjson");
+    updateParams.put("updates", usersJSONArray);
+    postHttpRequest(cardiffServer + usersLayer + "applyEdits", updateParams);
+  }
+  
+  public String queryEvents() {
+    String token = getToken();
+    
+    Map<String, Object> queryParams = new LinkedHashMap<>();
+    queryParams.put("token", token);
+    queryParams.put("f", "pjson");
+    queryParams.put("where", "oid > 0");
+    queryParams.put("outfields", "*");
+    
+    String result = postHttpRequest(cardiffServer + eventLayer + "query", queryParams);
     return result;
   }
   
   public static void main(String[] args) {
-
-  /*
-    Map<String, Object> params = new LinkedHashMap<>();
-    //params.put("");
-
-    HttpRequest("https://cardiffportal.esri.com/server/rest/services/Hosted/EventServiceWebMercator/FeatureServer/0/query", params);
-    */
-    
-    String token = getToken();
-    System.out.println(token);
+    RestRequest request = new RestRequest();
+    String token = request.getToken();
+    //System.out.println(request.updateUserPostion(3, 0.0, 0.0));
+    System.out.println(request.queryEvents());
   }
 }
